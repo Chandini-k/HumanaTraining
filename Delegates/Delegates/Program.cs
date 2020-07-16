3ae367e9385e0775b8f0d562f3557b5ba34e1392
CHANDINI KONDAPAVULURI <chandini22051998@gmail.com>
2020-07-16 09:09:09 +0530

    handson

---
+++ Delegates/Delegates/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Delegates
+{
+    class Program
+    {
+        public delegate void TestDelegate(int x,int y);
+        static void Main(string[] args)
+        {
+            TestDelegate testDelegate1 =Add;
+            TestDelegate testDelegate2 = Mul;
+            testDelegate1(2, 4);
+            testDelegate2(2, 2);
+        }
+        public static void Add(int a, int b)
+        {
+            Console.WriteLine("Adding Two Number " + (a + b));
+        }
+        public static void Mul(int a, int b)
+        {
+            Console.WriteLine("Adding Two Number " + (a * b));
+        }
+    }
+}
---
+++ Delegates/Events/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Events
+{
+    class Program
+    {
+        public delegate int Delegate(int a, int b);
+        class EventProgram
+        {
+            event Delegate myEvent;
+            public EventProgram()
+            {
+                this.myEvent += new Delegate(this.Add);
+            }
+            public int Add(int a,int b)
+            {
+                return a + b;
+            }
+            static void Main(string[] args)
+            {
+                EventProgram eventProgram = new EventProgram();
+                int result = eventProgram.myEvent(2, 2);
+                Console.WriteLine("Adding no: " + result);
+            }
+
+        }
+    }
+}
