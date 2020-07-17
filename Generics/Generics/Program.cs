f773111da258ec481e30bf492cf897b18fd879d8
CHANDINI KONDAPAVULURI <chandini22051998@gmail.com>
2020-07-17 09:17:12 +0530

    handson

---
+++ Generics/Generics/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(AreEqual("Tod", "Vachev"));
+            Console.WriteLine(AreEqual(5, 5));
+            Console.WriteLine(AreEqual(true, false));
+            Console.WriteLine(new string('-', 40));
+            int[] a = new int[] {2,1,3,6,5,9,8};
+            Console.WriteLine(string.Join(", ", Sort(a)));
+
+        }
+        public static bool AreEqual<T>(T a, T b) where T : IComparable<T>
+        {
+            return a.CompareTo(b) == 0;
+        }
+        public static T[] Sort<T>(T[] c) where T : IComparable<T>
+        {
+            for (int i = 0; i < c.Length; i++)
+            {
+                for (int j = i + 1; j < c.Length; j++)
+                {
+                    if (c[i].CompareTo(c[j]) > 0)
+                    {
+                        T temp = c[i];
+                        c[i] = c[j];
+                        c[j] = temp;
+                    }
+                }
+            }
+            return c;
+        }
+    }
+}
