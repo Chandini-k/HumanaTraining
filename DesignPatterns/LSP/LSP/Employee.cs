6b99fdaec7575ac19b28d42ffea551ae41f62bd8
CHANDINI KONDAPAVULURI <chandini22051998@gmail.com>
2020-07-22 20:17:59 +0530

    handson

---
+++ DesignPatterns/LSP/LSP/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LSP
+{
+    public class Employee : BaseEmployee, IManaged
+    {
+        public IEmployee Manager { get; set; } = null;
+
+        public void AssignManager(IEmployee manager)
+        {
+           
+            Manager = manager;
+        }
+    }
+}
---
+++ DesignPatterns/LSP/LSP/IManaged.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LSP
+{
+    public interface IManaged : IEmployee
+    {
+        IEmployee Manager { get; set; }
+        void AssignManager(IEmployee manager);
+    }
+}
