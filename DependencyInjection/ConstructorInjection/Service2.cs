facc58c263254bd0dfbd4ceafb4d1d58a2a41874
Chandini-K <chandini22051998@gmail.com>
2020-08-16 17:43:02 +0530

    handson

---
+++ DependencyInjection/ConstructorInjection/Service2.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConstructorInjection
+{
+    public class Service2 : IService
+    {
+        public void Serve() { Console.WriteLine("Service1 Called"); }
+    }
+}
---
+++ DependencyInjection/PropertyOrSetterInjection/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PropertyOrSetterInjection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
